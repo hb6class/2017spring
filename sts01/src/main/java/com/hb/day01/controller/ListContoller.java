1ae01910ef0381a26acb5d7e8d7e7d2d1d7185f6
hb06class <hb06class@gmail.com>
2017-12-28 11:14:06 +0900

    sts tool project(2)

---
+++ sts01/src/main/java/com/hb/day01/controller/ListContoller.java
@@ -0,0 +1,38 @@
+package com.hb.day01.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.hb.day01.model.GuestDao;
+
+// POJO
+
+@Controller
+public class ListContoller {
+	@Autowired
+	GuestDao guestDao;
+
+	@RequestMapping("/list.do")
+	public ModelAndView execute(HttpServletRequest req
+			, HttpServletResponse res) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("list", guestDao.selectAll());
+		mav.setViewName("list");
+		
+		return mav;
+	}
+}
+
+
+
+
+
+
+
+
+
